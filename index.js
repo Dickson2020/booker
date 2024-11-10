c78b7b8b18336cd843619d95e9d6e94f6cd43246
dickson <anoibidickson@gmail.com>
2024-11-10 19:42:28 +0100

    change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 3000;
 const { Pool } = require('pg')
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
