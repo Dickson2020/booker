aaa9b11f97047f91af0c2ec73dcf854e4b618987
Dickson <anoibidickson@gmail.com>
2025-03-15 10:53:32 -0700

    push

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ const cors = require('cors');
 // Increase the limit for the body-parser middleware
 app.use(express.json({ limit: '50mb' })); // Parse JSON bodies with a limit of 50MB
 app.use(express.urlencoded({ extended: true, limit: '50mb' })); // Parse URL-encoded bodies with a limit of 50MB
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
+app.use(express.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true }));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 
 /*
@@ -97,14 +110,6 @@ async function initializeStripe() {
 
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
-app.use(express.json()); // Parse JSON bodies
-app.use(express.urlencoded({ extended: true }));
 
 // This example sets up an endpoint using the Express framework.
 app.post('/webhook', express.json({ type: 'application/json' }), async (request, response) => {
